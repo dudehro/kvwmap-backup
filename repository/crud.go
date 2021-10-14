488607bc6438dd580037bd55d3db961f30300a59
gkaemmert <public@kaemmert.de>
2021-10-14 16:28:48 +0200

    crud komplett für tar implementiert

--- repository/crud.go
+++ repository/crud.go
@@ -5,22 +5,38 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+//        "fmt"
 )
 
-func Load_backup_config(file string) *json_struct.GdiBackup {
-	content, err := ioutil.ReadFile(file)
+func Load_backup_config(file string) (*structs.GdiBackup, error) {
+	content, err := ioutil.ReadFile("./backup-config/"+file)
+	var payload structs.GdiBackup
 	if err != nil {
-		log.Fatal("Fehler beim öffnen: ", err)
+		log.Print("Fehler beim öffnen: ", err)
+	} else {
+		err = json.Unmarshal(content, &payload)
+		if err != nil {
+			log.Print("Fehler beim unmarshalen: ", err)
+		}
 	}
-
-	var payload json_struct.GdiBackup
-	err = json.Unmarshal(content, &payload)
-       	if err != nil {
-		log.Fatal("Fehler beim unmarshalen: ", err)
-	}
-//	log.Printf("Erfolg")
-	return &payload
+	return &payload, err
 }
 
-func Write_json(j *json_struct.GdiBackup){
+func Write_json(j *structs.GdiBackup, file string) structs.Request {
+	data, err := json.MarshalIndent(j, "", " ")
+//        fmt.Printf("Write_json output: \n %s\n", string(data))
+	s := structs.Request{}
+	s.Success=true
+	if err != nil {
+		s.Success = false
+		s.Errors = append(s.Errors, err.Error())
+	}
+	err = ioutil.WriteFile("./backup-config/"+file, data, 0644)
+	if err != nil {
+		s.Success = false
+		s.Errors = append(s.Errors, err.Error())
+	}
+	
+	return s
+
 }
--- usecases/api_usescases.go
+++ usecases/api_usescases.go
@@ -7,10 +7,11 @@ import (
 //	"fmt"
 	"log"
 	"strings"
+	"strconv"
 )
 
 func List_backup_configs() []string {
-	root := "./"
+	root := "./backup-config/"
 	var files []string
 	dir_content, err := ioutil.ReadDir(root)
         if err != nil {
@@ -24,6 +25,111 @@ func List_backup_configs() []string {
 	return files
 }
 
-func Load_backup_config(file string) *json_struct.GdiBackup {
+func Load_backup_config(file string) (*structs.GdiBackup, error) {
 	return crud.Load_backup_config(file)
+}
+
+func Save_Backup_Config(data *structs.GdiBackup, file string) structs.Request {
+	s := structs.Request{}
+	write_result := crud.Write_json(data, file)
+	if write_result.Success {
+		s.Success = true
+	} else {
+		s.Errors = write_result.Errors
+		s.Success = false
+	}
+	return s
+}
+
+func Load_Taritem(file string, id string) (*structs.TarItem, structs.Request) {
+	data, err := Load_backup_config(file)
+	s := structs.Request{}
+	if err != nil {
+		s.Errors = append(s.Errors, err.Error())
+		s.Success = false
+		return nil, s
+	}
+        id_int, err := strconv.Atoi(id)
+        if err != nil {
+                s.Errors = append(s.Errors, err.Error())
+		s.Success = false
+		return nil, s
+        }
+	taritem := &structs.TarItem{}
+	if len(data.Tar) >= id_int {
+		taritem = data.Tar[id_int]
+		s.Success = true
+	}
+	return taritem, s
+}
+
+
+
+func Save_TarItem(item structs.TarItem, file string) structs.Request {
+	data, err := Load_backup_config(file)
+	s := structs.Request{}
+	if err != nil {
+		s.Errors = append(s.Errors, err.Error())
+		s.Success = false
+		return s
+	}
+
+	found := false
+	for i, taritem := range data.Tar {
+		if taritem.Source == item.Source {
+			found = true
+			data.Tar[i] = &item	//update
+		}
+	}
+	if !found {				//insert
+		tararray := data.Tar		//refactor
+		tararray = append(tararray, &item)
+		data.Tar = tararray
+	}
+
+	req := Save_Backup_Config(data, file)
+	if req.Success {
+		s.Success = true
+	} else {
+		s.Errors = append(s.Errors, req.Errors[0])
+		s.Success = false
+	}
+	return s
+}
+
+func Delete_TarItem(file string, id string) structs.Request {
+	data, err := Load_backup_config(file)
+	s := structs.Request{}
+	if err != nil {
+		s.Errors = append(s.Errors, err.Error())
+		s.Success = false
+		return s
+	}
+        id_int, err := strconv.Atoi(id)
+        if err != nil {
+                s.Errors = append(s.Errors, err.Error())
+                s.Success = false
+                return s
+        }
+	if id_int <= len(data.Tar) {
+
+		copy(data.Tar[id_int:], data.Tar[id_int+1:])
+		data.Tar[len(data.Tar)-1] = nil
+		data.Tar = data.Tar[:len(data.Tar)-1]
+
+		s.Success = true
+	} else {
+		s.Errors = append(s.Errors, "Es existiert kein Eintrag mit ID="+id)
+		s.Success = false
+	}
+
+	req := Save_Backup_Config(data, file)
+	if req.Success {
+		s.Success = true
+	} else {
+		s.Errors = append(s.Errors, req.Errors[0])
+		s.Success = false
+	}
+
+	return s
 }
