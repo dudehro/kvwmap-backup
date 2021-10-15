b978af3f71628c225f332fbae6c7cd8cb9defbd8
gkaemmert <public@kaemmert.de>
2021-10-15 17:11:49 +0200

    build and run app with docker

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,5 +1,5 @@
-docker rm backup-interface
-docker build -t gkaemmert/backup-interface:1.0 .
-docker run -p 8082:8082 \
--v /home/work/go/src/github.com/kvwmap-backup/backup-config/:/backup-interface/backup-config \
---name backup-interface gkaemmert/backup-interface:1.0
+docker rm -f kvwmap-backup
+docker build -t gkaemmert/kvwmap-backup:1.0 .
+docker run -d  -p 8082:8082 \
+-v /home/work/github.com/kvwmap-backup/backup-config/:/app/backup-config \
+--name kvwmap-backup gkaemmert/kvwmap-backup:1.0
