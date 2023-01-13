505aa29e136d95dc3cf3a4f755981f88aebc2dd8
gkaemmert <georg.kaemmert@gdi-service.de>
2023-01-13 11:30:13 +0100

    overlayfs mountet Snapshots bis zur letzten Vollsicherung

--- overlayfs.sh
+++ overlayfs.sh
@@ -3,13 +3,32 @@
 backupDir=${1}
 mountDir=${2}
 
-mountBranches=$(find ${backupDir} -mindepth 1 -maxdepth 1 -type d -exec echo {} \;|sort -r|tr "\n" ":")
-mountBranches=${mountBranches:0:${#mountBranches}-1}
+mountBranches=""
+i=0
+while read SNAPSHOT
+do
+    ((i=i+1))
+    if [ -z "$mountBranches" ]; then
+        mountBranches=${SNAPSHOT}
+    else
+        mountBranches=${mountBranches}:${SNAPSHOT}
+    fi
+
+    if [ -f ${SNAPSHOT}/full.backup ]; then
+        break 1
+    fi
+done < <(find ${backupDir} -mindepth 1 -maxdepth 1 -type d|sort -r)
 
 mountpoint -q ${mountDir}
 if [ "$?" -eq 0 ]; then
     umount ${mountDir}
 fi
 
-echo "mount -t overlay overlay -o lowerdir=${mountBranches} ${mountDir}"
-mount -t overlay overlay -o lowerdir=${mountBranches} ${mountDir}
+if [[ "$i" -eq 1 ]]; then
+    MOUNTCMD="mount --bind ${mountBranches} ${mountDir}"
+else
+    MOUNTCMD="mount -t overlay overlay -o lowerdir=${mountBranches} ${mountDir}"
+fi
+
+echo "$MOUNTCMD"
+$MOUNTCMD
