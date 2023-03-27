ac096efe4406425511a6b6e513e356c0b246f79e
Peter Korduan <peter.korduan@gdi-service.de>
2023-03-27 16:01:14 +0200

    zentrale backup.conf für rsync+overlayfs

--- overlayfs.sh
+++ overlayfs.sh
@@ -1,30 +1,77 @@
 #!/bin/bash
 
-backupDir=${1}
-mountDir=${2}
-
-mountBranches=""
-i=0
-while read SNAPSHOT
-do
-    ((i=i+1))
-    if [ -z "$mountBranches" ]; then
-        mountBranches=${SNAPSHOT}
-    else
-        mountBranches=${mountBranches}:${SNAPSHOT}
-    fi
-
-    if [ -f ${SNAPSHOT}/full.backup ]; then
-        break 1
-    fi
-done < <(find ${backupDir} -mindepth 1 -maxdepth 1 -type d|sort -r)
-
-mountpoint -q ${mountDir}
-if [ "$?" -eq 0 ]; then
-    umount ${mountDir}
+MODUS=${1}
+if [ -z "$MODUS" ]; then
+    echo "Argument fehlt: overlayfs.sh [diff|incr] [config]"
 fi
 
-if [[ "$i" -eq 1 ]]; then
+Config=$HOME/.config/backup/backup.conf
+if [ -n "$2" ]; then
+    Config="$2"
+fi
+
+if [ ! -f "$Config" ]; then
+    echo "Konfiguration $Config existiert nicht."
+    exit 1
+fi
+
+# shellcheck source=/root/.config/backup/backup.conf
+source "$Config"
+
+if [ -z "$SnapshotDir" ] || [ -z "$OverlayDir" ]; then
+    echo "Konfiguration unvollständig. Benötige SnapshotDir und OverlayDir!"
+    exit 1
+fi
+
+backupDir="$SnapshotDir"
+mountDir="$OverlayDir"
+DiffIncr="$MODUS"   #defaults to "incr"
+
+if [ -z "$DiffIncr" ]; then
+    DiffIncr="incr"
+fi
+
+# select snapshots
+mountBranches=""
+branchCount=0
+while read SNAPSHOT
+do
+    if [ "$DiffIncr" = "diff" ]; then
+        if [ -z "$mountBranches" ]; then
+            ((branchCount=branchCount+1))
+            mountBranches=${SNAPSHOT}
+        fi
+
+        if [ -f "$SNAPSHOT/full.backup" ]; then
+            ((branchCount=branchCount+1))
+            if [ -z "$mountBranches" ]; then
+                mountBranches=${SNAPSHOT}
+            else
+                mountBranches=${mountBranches}:${SNAPSHOT}
+            fi
+        fi
+
+    elif [ "$DiffIncr" = "incr" ]; then
+        ((branchCount=branchCount+1))
+        if [ -z "$mountBranches" ]; then
+            mountBranches=${SNAPSHOT}
+        else
+            mountBranches=${mountBranches}:${SNAPSHOT}
+        fi
+    fi
+
+    if [ -f "$SNAPSHOT/full.backup" ]; then
+        break 1
+    fi
+done < <(find "$backupDir" -mindepth 1 -maxdepth 1 -type d|sort -r)
+
+# unmount
+if mountpoint -q "$mountDir"; then
+    umount "$mountDir"
+fi
+
+# build command
+if [[ "$branchCount" -eq 1 ]]; then
     MOUNTCMD="mount --bind ${mountBranches} ${mountDir}"
 else
     MOUNTCMD="mount -t overlay overlay -o lowerdir=${mountBranches} ${mountDir}"
@@ -32,3 +79,4 @@ fi
 
 echo "$MOUNTCMD"
 $MOUNTCMD
+
