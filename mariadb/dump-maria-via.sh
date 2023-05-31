4e6ceb66dcd370a6a408dcca90b6aa857a30db24
Georg <public@kaemmert.de>
2023-05-31 10:23:49 +0200

    mariadb: dump über mysqldump in anderem Container

---
+++ mariadb/dump-maria-via.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ${1}
+export CONTAINER
+
+MYSQLUSER=$(grep MYSQL_USER "$CREDENTIALS_FILE" | cut -d "'" -f 4)
+MYSQLPW=$(grep MYSQL_PASSWORD "$CREDENTIALS_FILE" | cut -d "'" -f 4)
+EXITCODE=0
+
+[ ! -d ${TARGET} ] && mkdir -p ${TARGET}
+
+for DB in "${DATABASES[@]}"
+do
+    FILENAME=${CONTAINER}.${DB}.dump
+    docker exec ${VIA_CONTAINER} bash -c "mysqldump -h mysql --single-transaction --user=$MYSQLUSER --databases ${DB} --password=\"$MYSQLPW\" > /var/www/mysqldumps/${FILENAME}"
+    if [ $? -ne 0 ]; then
+        EXITCODE=1
+    fi
+done
+
+DUMPDIR=$(docker inspect --format "{{json .Mounts}}" ${VIA_CONTAINER} | jq -r '.[]|select(.Destination=="/var/www").Source')
+mv ${DUMPDIR}/mysqldumps/${CONTAINER}.*.dump ${TARGET}
+if [ $? -ne 0 ]; then
+    EXITCODE=3
+fi
+
+exit ${EXITCODE}
